4a774b240929b64b0ac7b23f7b9aed558f08e732
Taha Abdulrahmman <tahaabdalrahman97@gmail.com>
2022-06-24 23:52:02 +0200

    upgrade my site to the best

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Start move text
+AOS.init({ duration: 1100, once: true });
+// Emd  move text
+let section = document.querySelector(".skills");
+let spans = document.querySelectorAll(".skills span");
+
+window.onscroll = function () {
+  if ((window.scrollY >= section.offsetTop - 200)) {
+    console.log("Reached Section Three");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+// Stast toggle
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let close = document.querySelector(".close");
+
+toggler.onclick = function () {
+  nav.classList.add("open");
+};
+//  End toggle
+close.onclick = function () {
+  this.parentElement.classList.remove("open");
+};
